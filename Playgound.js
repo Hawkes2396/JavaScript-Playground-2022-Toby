91ce11d3d67555e2e9e6d2f6d15135fa6b0b9698
Hawkes2396 <Tobias-Hawkes@live.nmit.ac.nz>
2022-03-09 14:04:13 +1300

    week-3

--- Playgound.js
+++ Playgound.js
@@ -2,3 +2,9 @@ Playgound
 //TODO:
 
 //NOTE:
+/*
+creat app.js, then creat package.json by'npm init -y' 
+save changes then you on the correct branch; git add ., 
+git commit -m "file name", 
+git push 'if main branch origin main if not main branch then brach name'
+*/
