206e2df5f73e2bf867fbfdbe29f4a20656b2e1e9
Hawkes2396 <Tobias-Hawkes@live.nmit.ac.nz>
2022-04-13 11:46:43 +1200

    Playgound.js

--- Playgound.js
+++ Playgound.js
@@ -232,3 +232,11 @@ console.log(getIndex(["Q","W","R"], "W"))// -> 1
 
 var NEWmessage = "Welcome to Hell"
 console.log(NEWmessage)
+
+// const array1 = [1, 4, 9, 16];
+
+// // pass a function to map
+// const map1 = array1.map(x => x * 2);
+
+// console.log(map1);
+// // expected output: Array [2, 8, 18, 32]
