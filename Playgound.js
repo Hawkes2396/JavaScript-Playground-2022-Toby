e608c0d3f6c4d31096e39fcd6fdca5647d4003be
Hawkes2396 <Tobias-Hawkes@live.nmit.ac.nz>
2022-05-11 15:11:11 +1200

    Commitimg work on this branch week 6

--- Playgound.js
+++ Playgound.js
@@ -214,131 +214,442 @@ the array values all in number data type
 // let even = true
 // let odd = false
 // console.log("trueOrFalse")
-//209 to 217 failed unfortunetly
-function isEven(string) {
-    var len = string.length
-    return (len/2 == Math.round(len/2))
-}
+// //209 to 217 failed unfortunetly
+// function isEven(string) {
+//     var len = string.length
+//     return (len/2 == Math.round(len/2))
+// }
 
-console.log(isEven("Home"));
+// console.log(isEven("Home"));
 
-var isEven = (string) => string.length % 2 == 0
+// var isEven = (string) => string.length % 2 == 0
 
 
-function getIndex (array, string) {
-    return array.indexOf(string)
-}
-console.log(getIndex(["Q","W","R"], "W"))// -> 1
+// function getIndex (array, string) {
+//     return array.indexOf(string)
+// }
+// console.log(getIndex(["Q","W","R"], "W"))// -> 1
 
-var NEWmessage = "Welcome to Hell"
-console.log(NEWmessage)
+// var NEWmessage = "Welcome to Hell"
+// console.log(NEWmessage)
 
-// const array1 = [1, 4, 9, 16];
+// // const array1 = [1, 4, 9, 16];
 
-// // pass a function to map
-// const map1 = array1.map(x => x * 2);
+// // // pass a function to map
+// // const map1 = array1.map(x => x * 2);
 
-// console.log(map1);
-// // expected output: Array [2, 8, 18, 32]
+// // console.log(map1);
+// // // expected output: Array [2, 8, 18, 32]
 
-let arr = [
-    {id:"toby"},
-    {id:7010},
-    {id:"hawkes"},
-    {id:2022},
-]
+// let arr = [
+//     {id:"toby"},
+//     {id:7010},
+//     {id:"hawkes"},
+//     {id:2022},
+// ]
 
-let invalidEntries = 0
+// let invalidEntries = 0
 
-function filterByID(item) {
-  if (Number.isFinite(item.id) && item.id !== 0) {
-    return true
-  }
-  invalidEntries++
-  return false;
-}
+// function filterByID(item) {
+//   if (Number.isFinite(item.id) && item.id !== 0) {
+//     return true
+//   }
+//   invalidEntries++
+//   return false;
+// }
 
-let arrByID = arr.filter(filterByID)
+// let arrByID = arr.filter(filterByID)
 
-console.log('Filtered Array\n', arrByID)
+// console.log('Filtered Array\n', arrByID)
 
-console.log('Number of Invalid Entries = ', invalidEntries)
+// console.log('Number of Invalid Entries = ', invalidEntries)
 
-function onlyNumbers(arr) {
-    return arr.filter(val => Number.isInteger(val))
-}
+// function onlyNumbers(arr) {
+//     return arr.filter(val => Number.isInteger(val))
+// }
 
-function addByOne(num){
-    return num + 1
-}
-console.log(addByOne(3))
+// function addByOne(num){
+//     return num + 1
+// }
+// console.log(addByOne(3))
 
-const booStr1 = new Boolean(true);
+// const booStr1 = new Boolean(true);
 
-console.log(booStr1.toString())
+// console.log(booStr1.toString())
 
-function greaterThan(num1,num2,arr){
-    let filtered = []
-    let n = arr.length
-    for (var i = 0; i < n ;i++){
-        if((arr[i]>num1) &&(arr[i]<num2)) {
-            filtered.push(arr[i])
-        }
-    }
-    return filtered
-}
+// function greaterThan(num1,num2,arr){
+//     let filtered = []
+//     let n = arr.length
+//     for (var i = 0; i < n ;i++){
+//         if((arr[i]>num1) &&(arr[i]<num2)) {
+//             filtered.push(arr[i])
+//         }
+//     }
+//     return filtered
+// }
 
-//let filtered = [3,4,5,6,7].filter(greaterThan)
-console.log(greaterThan(2,7,[1,2,3,4,5,6,7,8,9,10]))
+// //let filtered = [3,4,5,6,7].filter(greaterThan)
+// console.log(greaterThan(2,7,[1,2,3,4,5,6,7,8,9,10]))
 
-function greaterThan(num1, num2, arr) {
-    return arr.filter(val => val > num1 && val < num2)
-}
-console.log(greaterThan(2,7,[1,2,3,4,5,6,7,8,9,10]))
+// function greaterThan(num1, num2, arr) {
+//     return arr.filter(val => val > num1 && val < num2)
+// }
+// console.log(greaterThan(2,7,[1,2,3,4,5,6,7,8,9,10]))
 
-// class className{
-//     constructor(param1, param2, ....){
-//         this.prop1 = param1
-//         this.prop2 = param2
+// // class className{
+// //     constructor(param1, param2, ....){
+// //         this.prop1 = param1
+// //         this.prop2 = param2
+// //     }
+// // }
+
+// class Person{
+//     constructor(name, age,){
+//         this.name = name
+//         this.age = age
 //     }
 // }
 
-class Person{
-    constructor(name, age,){
-        this.name = name
-        this.age = age
-    }
-}
+// let personOne = new Person('Toby',123)
+// let personTwo = new Person('Kenny',44)
+// let personThree = new Person('Max',56)
+// let personFour = new Person('Sam',33)
 
-let personOne = new Person('Toby',123)
-let personTwo = new Person('Kenny',44)
-let personThree = new Person('Max',56)
-let personFour = new Person('Sam',33)
+// console.log(personTwo.name)
 
-console.log(personTwo.name)
+// // class Student{
+// //     constructor(name, id, age,){
+// //     this.name = name
+// //     this.id = id
+// //     this.age = age
+// //     }
+// // }
 
-// class Student{
-//     constructor(name, id, age,){
+// // const first_year = new Student()
+
+// // first_year.name
+
+// class Greeting {
+//     constructor(firstName, lastName){
+//         this.firstName = firstName
+//         this.lastName = lastName
+//     }
+//     greet(){
+//         return 'Hello ${this.firstName} ${this.lastName}'
+//     }
+// }
+
+// const new_person = new Greeting('Toby','Hawkes')
+
+// console.log(new_person)
+
+
+// function findMiniMax(arr){
+//     let array=[];
+//     let min = arr[0]
+//     let max = arr[0]
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i] < min){
+//             min = arr[i]
+//         }
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     array.push(min)
+//     array.push(max)
+
+//     return array
+// }
+
+// console.log(findMiniMax([1,2,3,4,5]))
+
+//below is problem for 1st and 2nd solution.
+// arraysConcat([1,2,3,4,5], [6,7,8,9,]) -> [1,2,3,4,5,6,7,8,9]
+
+// const firstOne = [1,2,3,4,5]
+// const secondOne = [6,7,8,9]
+// const combinBothEm = firstOne.concat(secondOne);
+// console.log(firstOne)
+// console.log(secondOne)
+// console.log(combinBothEm)
+// 1st solution
+
+// function concatArray(...args) {
+//     return [].concat(...args)
+// }
+
+// let concatArray = (...a) => a.flat(Infinity)//2nd solution
+
+//Creat a function that takes an array of numbers,
+//a string and return an array of number as per the following rules:
+//ArrayRules([4,3,2,1], "Asc") -> [1,2,3,4]
+//ArrayRules([5,6,7,8,9], "Des") -> [9,8,7,6,5]
+//ArrayRules([4,3,2,1], "None") -> [4,3,2,1]
+
+// function ArrayRules (arr,str ) {
+
+//     switch(str){
+//         case 'Asc':
+//             return arr.sort((a,b) => a-b)
+
+//             case 'Des':
+//                 return arr.sort((a,b) => b-a)
+
+//                 default:
+//                     return arr
+//     }
+// }
+
+// console.log(ArrayRules)
+
+//Creat a function that returns the total number of items in nested arrays.
+
+// const arr1 = ([1,[2,3]])
+// console.log(arr1.flat());
+// const arr2 = ([1,[2,[3,4]]])
+// console.log(arr2.flat(2));
+// const nestedArrays = arr => arr.flat(Infinity).Length
+
+// console.log(arr1.flat(Infinity));
+
+//example of class below
+// class Rectangle {
+//     constructor(height, width) {
+//       this.height = height;
+//       this.width = width;
+//     }
+//   }
+
+// write a function that divides an array into sub arrays based on n number giving
+// where n is the length of each sub array.
+
+/*
+subarray([1,2,3,4,5,6,7,8,9,10],2)->[[1,2],[3,4],[5,6],[7,8],[9,10]]
+*/
+
+// function subarrays(arr,n){
+//   let x = []
+//   let y = []
+
+//   for (let i = 0; i < arr.length; i++) {
+//     x.push(arr[i])
+//     if(x.length%n === 0) {
+//       y.push(x)
+//       x=[]
+//     }
+//     else {
+//       y.push(x)
+//     }
+//     return [... new Set(x)]
+//   }
+// }
+
+// console.log(subarrays([1,2,3,4,5,6,7,8,9,10],2))
+
+// for (let i = 0; i < arr.length; i++) {
+// i index starts at 0 i++ is index +1 meaning it show appair as 1 not 0
+
+
+// const buffer = new ArrayBuffer(8);
+// const Uint8Array = new Uint10Array(buffer, 2);
+
+// console.log(uint8.byteLength);
+
+// const numbers = [3,6,9];
+
+// const arr1 = [1,3,5,7,9,]
+
+// const arr2 = [2,4,6,8,10,]
+
+// const result = numbers.filter(numbers => numbers.length > 8);
+
+// console.log(result);
+
+// const common = (array1, array2) => {
+//   return array1.filter(elem => array2.includes(elem))
+// }
+// console.log(common([1,3,4,5],[1,2,6,7,4,8,9,3]));
+
+
+//the above code failed
+
+// function isBigEnough(value) {
+//   return value >= 10
+// }
+
+// let filtered = [12, 5, 8, 130, 44].filter(isBigEnough)
+
+// console.log(filtered)
+
+//The experesion above is fix! :)
+
+// let numberArray = [515,314,44,3];
+
+// function reorder(arr){
+//   return arr;{
+//     numberArray.join(); 
+//     numberArray.sort();
+//   }
+// }
+
+// console.log()
+
+
+// function redoes(arr, order) {
+//   let newArr = [];
+//   switch(order) {
+//   case 'des':
+//   for(let i = 0 ; i < arr.length; i++) {
+//      newArr.push (arr.sort((a, b) => b-a));
+//   };
+//   return newArr
+//   case 'asc':
+//   for(let i = 0; i < arr.length; i++) {
+//       newArr.push (arr.sort((a, b) => a-b));
+//   };
+//   return newArr
+// }
+// }
+// console.log(redoes([515, 341, 44, 3], 'asc'))
+
+//not my work above. and didn't work, but interesting.
+
+/*
+Given an object with students and the grades that they made on the tests that
+they took, determine which student has the best Test Average.
+
+The key will be the student's name and the value will be an array of their 
+grades you will only have to return the student's name. you do not need to
+return their Test Average.
+*/
+
+// let Ali = [67,84,75,63];
+// let James = [87,98,64,71];
+// let Matt = [90,58,73,86];
+// let mixedNumericArray = [67,84,75,63,87,98,64,71,90,58,73,86]
+
+// function compareStudents(a, b) {
+//   return a - b;
+// }
+
+// Ali.join(); 
+// Ali.sort(); 
+ 
+// James.join(); 
+// James.sort(); 
+// James.sort(compareNumbers); 
+
+// Matt.join(); 
+// Matt.sort(); 
+// Matt.sort(compareNumbers); 
+
+// mixedNumericArray.join(); 
+// mixedNumericArray.sort(); 
+// mixedNumericArray.sort(compareNumbers); 
+
+// console.log(mixedNumericArray)
+
+// class studentTestScore {
+//   constructor(name, scores){
 //     this.name = name
-//     this.id = id
-//     this.age = age
-//     }
+//     this.scores = scores
+//   }
 // }
 
-// const first_year = new Student()
+// let student1 = studentTestScore("Ali",[67,84,75,63])
+// let student2 = studentTestScore("James",[87,98,64,86])
+// let student3 = studentTestScore("Matt",[90,58,73,86])
 
-// first_year.name
+// students.sort((firstItem, secondItem) => firstItem.scores - secondItem.scores);
 
-class Greeting {
-    constructor(firstName, lastName){
-        this.firstName = firstName
-        this.lastName = lastName
-    }
-    greet(){
-        return 'Hello ${this.firstName} ${this.lastName}'
-    }
-}
+// let user = new Object();
 
-const new_person = new Greeting('Toby','Hawkes')
+// console.log; user
 
-console.log(new_person)
+
+// let user = {
+//   name: 'sam',
+//   age: 30
+// };
+
+// console.name
+// console.age
+
+
+// let product = new Object()
+// console.log(product)
+// product.name = 'Laptop'
+// product.price = 1200
+
+// console.log(product)
+
+// product.price = 1000
+
+// delete product.name
+// console.log(product)
+
+// let user = {name: "John", age:30};
+
+// console.log(user)
+
+//alert method can be used in browser console. e.g. alert("age" in user)
+
+// let user = {
+//   name: "John",
+//   age: 30,
+//   isAdmin: true
+// };
+
+// for (let key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Peter: 130
+// }
+
+// let user = { name: "John"};
+// let admin = user; 
+
+// console.log(admin)
+
+// const user = {
+//   name: "John",
+//   age: 21
+// };
+// user.name = "Tobias"
+// user.age = 23
+// console.log(user.name);
+// console.log(user.age);
+
+// let user = {
+//   name: "John",
+//   age: 30
+// };
+// let clone = Object.assign({}, user);
+
+// //now clone is a fully independent clone
+// clone.name = "David";//changed the data in it
+
+// console.log(user.name);
+// console.log(clone.name);
+
+// let user = {
+//   name: "John",
+//   age: 30
+// };
+// user.sayHi = function () {
+//   console.log("Hello!");
+// };
+
+// user.sayHi();
+// console.log(user)
+
+
+
+
